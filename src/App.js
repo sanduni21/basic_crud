66cac6d4082dce13c6ceeb7cbe95fcc0fa6a9439
sanduni <schathurika98@gmail.com>
2021-11-02 21:26:17 +0530

    create incriment
    create

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Display from './Component/display'
+
 
 function App() {
+ 
+  const [state, setState] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Display value={state}/>
+     <Display value={state}/>
+     <Display value={state}/>
+     <Display value={state}/>
+
+      <button onClick={()=>{
+        setState(state+1)
+      }}>
+        increment
+      </button>
     </div>
   );
 }
---
+++ src/Component/display.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Display = (props) =>{
+
+    return(
+        <div>
+                {props.value}
+
+        </div>
+    );
+}
+
+export default Display;
